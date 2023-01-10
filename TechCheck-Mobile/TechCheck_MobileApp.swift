144574e41850efabfa8764024a9d57e8d905b1fc
Stanislav Pyatnicyn <pyatnicyn.stanislav@gmail.com>
2023-01-10 23:45:08 +0300

    Initial Commit

---
+++ TechCheck-Mobile/TechCheck_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechCheck_MobileApp.swift
+//  TechCheck-Mobile
+//
+//  Created by user on 05.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct TechCheck_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TechCheck-MobileUITests/TechCheck_MobileUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TechCheck_MobileUITestsLaunchTests.swift
+//  TechCheck-MobileUITests
+//
+//  Created by user on 05.01.2023.
+//
+
+import XCTest
+
+final class TechCheck_MobileUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
